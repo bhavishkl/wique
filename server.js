1985706f3afdbb85e8502a67ed594a3a293c1cfe
bhavishkl <kodlibhavish@gmail.com>
2024-08-04 17:41:00 +0530

    new

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const { Server } = require('socket.io');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -13,14 +12,6 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
   });
 
-  const io = new Server(server);
-
-  io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
-    });
-  });
   const os = require('os');
 
   function getIPAddress() {
